e799044c3772a25066e572755d275ca86cf90f08
Jakub Psarski <psarski@student.agh.edu.pl>
2024-10-21 18:13:26 +0200

    vector2d - upperRight, lowerLeft, opposite, equals

--- oolab/src/main/java/agh/ics/oop/model/Vector2d.java
+++ oolab/src/main/java/agh/ics/oop/model/Vector2d.java
@@ -1,8 +1,10 @@
 package agh.ics.oop.model;
 
 public class Vector2d {
+
     private final int x;
     private final int y;
+
     public Vector2d(int x, int y) {
         this.x = x;
         this.y = y;
@@ -10,10 +12,10 @@ public class Vector2d {
     public String toString() {
         return "(" + x + ", " + y + ")";
     }
-    boolean precedes(Vector2d other) {
+    public boolean precedes(Vector2d other) {
         return x <= other.x && y <= other.y;
     }
-    boolean follows(Vector2d other) {
+    public boolean follows(Vector2d other) {
         return x >= other.x && y >= other.y;
     }
     public Vector2d add(Vector2d other) {
@@ -21,6 +23,18 @@ public class Vector2d {
     }
     public Vector2d subtract(Vector2d other) {
         return new Vector2d(x - other.x, y - other.y);
+    }
+    public Vector2d upperRight(Vector2d other) {
+        return new Vector2d(Math.max(x, other.x), Math.max(y, other.y));
+    }
+    public Vector2d lowerLeft(Vector2d other) {
+        return new Vector2d(Math.min(x, other.x), Math.min(y, other.y));
+    }
+    public Vector2d opposite() {
+        return new Vector2d(-x, -y);
+    }
+    public boolean equals(Vector2d other) {
+        return x == other.x && y == other.y;
     }
     public int getX() {
         return x;
