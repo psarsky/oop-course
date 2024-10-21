eb4a73b5bb79dc918a03595bc4caf5e77b40bfc3
Jakub Psarski <psarski@student.agh.edu.pl>
2024-10-21 18:04:04 +0200

    vector2d - gettery, toString, precedes, follows, add, subtract

--- oolab/src/main/java/agh/ics/oop/model/Vector2d.java
+++ oolab/src/main/java/agh/ics/oop/model/Vector2d.java
@@ -7,4 +7,25 @@ public class Vector2d {
         this.x = x;
         this.y = y;
     }
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+    boolean precedes(Vector2d other) {
+        return x <= other.x && y <= other.y;
+    }
+    boolean follows(Vector2d other) {
+        return x >= other.x && y >= other.y;
+    }
+    public Vector2d add(Vector2d other) {
+        return new Vector2d(x + other.x, y + other.y);
+    }
+    public Vector2d subtract(Vector2d other) {
+        return new Vector2d(x - other.x, y - other.y);
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
 }
