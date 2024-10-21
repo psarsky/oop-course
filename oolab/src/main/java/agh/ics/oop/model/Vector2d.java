c893919af134b8b80732067cfd989fbbd61b57e6
Jakub Psarski <psarski@student.agh.edu.pl>
2024-10-21 17:57:32 +0200

    vector2d - pola + konstruktor

---
+++ oolab/src/main/java/agh/ics/oop/model/Vector2d.java
@@ -0,0 +1,10 @@
+package agh.ics.oop.model;
+
+public class Vector2d {
+    private final int x;
+    private final int y;
+    public Vector2d(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
