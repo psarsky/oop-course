90a6fa69910d903568c3289514a02c233141537c
Jakub Psarski <psarski@student.agh.edu.pl>
2024-11-03 00:29:03 +0100

    dodana klasa Simulation

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -2,14 +2,21 @@ package agh.ics.oop;
 
 import agh.ics.oop.model.Animal;
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
 
 import java.util.List;
 
 public class World {
 
     public static void main(String[] args) {
-        Animal janusz = new Animal();
-        System.out.println(janusz.getPos());
+        System.out.println("START");
+
+        List<MoveDirection> directions = OptionsParser.parse(args);
+        List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
+        Simulation simulation = new Simulation(positions, directions);
+        simulation.run();
+
+        System.out.println("STOP");
     }
 
     private static void run(String[] args) {
