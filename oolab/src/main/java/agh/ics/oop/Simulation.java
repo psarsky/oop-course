908c9f1306826cc7601223d136bb3c325e64503f
Jakub Psarski <psarski@student.agh.edu.pl>
2024-11-09 01:08:15 +0100

    zrobiony lab4 oprócz testów

--- oolab/src/main/java/agh/ics/oop/Simulation.java
+++ oolab/src/main/java/agh/ics/oop/Simulation.java
@@ -2,6 +2,7 @@ package agh.ics.oop;
 
 import agh.ics.oop.model.Animal;
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.RectangularMap;
 import agh.ics.oop.model.Vector2d;
 
 import java.util.ArrayList;
@@ -11,23 +12,41 @@ public class Simulation {
 
     private final List<Animal> animals;
     private final List<MoveDirection> moves;
+    private final RectangularMap map;
 
-    public Simulation(List<Vector2d> initialPositions, List<MoveDirection> moves) {
+    public Simulation(List<Vector2d> initialPositions, List<MoveDirection> moves, RectangularMap map) {
+
         this.moves = moves;
+        this.map = map;
         animals = new ArrayList<>();
-        for (Vector2d initialPosition : initialPositions) {
-            animals.add(new Animal(initialPosition));
+
+        for(Vector2d pos : initialPositions) {
+            Animal animal = new Animal(pos);
+            if (this.map.place(animal)) {
+                animals.add(animal);
+                System.out.println(map);
+            }
         }
     }
 
     public void run() {
+
         int animalIndex = 0;
         int animalCount = animals.size();
+
         for (MoveDirection move : moves) {
-            animals.get(animalIndex).move(move);
-            System.out.println("Zwierzę " + animalIndex + ": " + animals.get(animalIndex).getPos());
+            if (animalIndex > animalCount - 1) {
+                break;
+            }
+            map.move(animals.get(animalIndex), move);
+            System.out.println(map);
             animalIndex = (animalIndex + 1) % animalCount;
         }
+    }
+
+    @Override
+    public String toString() {
+        return map.toString();
     }
 
     public Animal getAnimal(int index) {
--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,7 +1,7 @@
 package agh.ics.oop;
 
-import agh.ics.oop.model.Animal;
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.RectangularMap;
 import agh.ics.oop.model.Vector2d;
 
 import java.util.List;
@@ -12,22 +12,10 @@ public class World {
         System.out.println("START");
 
         List<MoveDirection> directions = OptionsParser.parse(args);
-        List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
-        Simulation simulation = new Simulation(positions, directions);
+        List<Vector2d> positions = List.of(new Vector2d(4,2), new Vector2d(4,1), new Vector2d(5,1), new Vector2d(1,2));
+        Simulation simulation = new Simulation(positions, directions, new RectangularMap(8, 2));
         simulation.run();
 
         System.out.println("STOP");
-    }
-
-    private static void run(String[] args) {
-        List<MoveDirection> dirs = OptionsParser.parse(args);
-        for (MoveDirection dir : dirs) {
-            switch (dir) {
-                case FORWARD -> System.out.println("Zwierzak idzie do przodu");
-                case BACKWARD -> System.out.println("Zwierzak idzie do tyłu");
-                case RIGHT -> System.out.println("Zwierzak idzie w prawo");
-                case LEFT -> System.out.println("Zwierzak idzie w lewo");
-            }
-        }
     }
 }
