9867b701c5f4ae0ddcf74ca8520c6e52568b4c7b
Jakub Psarski <psarski@student.agh.edu.pl>
2024-11-10 13:05:22 +0100

    dodana klasa TextMap, zmodyfikowana klasa World

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,8 +1,6 @@
 package agh.ics.oop;
 
-import agh.ics.oop.model.MoveDirection;
-import agh.ics.oop.model.RectangularMap;
-import agh.ics.oop.model.Vector2d;
+import agh.ics.oop.model.*;
 
 import java.util.List;
 
@@ -14,9 +12,15 @@ public class World {
         System.out.println("START");
 
         List<MoveDirection> directions = parse(args);
-        List<Vector2d> positions = List.of(new Vector2d(4,2), new Vector2d(4,1), new Vector2d(5,1), new Vector2d(1,2));
-        Simulation simulation = new Simulation(positions, directions, new RectangularMap(8, 2));
+        List<Vector2d> positions = List.of(new Vector2d(4, 2), new Vector2d(4, 1), new Vector2d(5, 1));
+        List<Object> animals = List.of(new Animal(new Vector2d(4, 2)), new Animal(new Vector2d(4, 1)), new Animal(new Vector2d(5, 1)));
+        Simulation<Object, Vector2d> simulation = new Simulation<>(animals, positions, directions, new RectangularMap<>(8, 8));
         simulation.run();
+
+        List<String> strings = List.of("Ala", "ma", "sowoniedźwiedzia");
+        List<Integer> ints = List.of(0, 0, 0);
+        Simulation<String, Integer> simulation2 = new Simulation<String, Integer>(strings, ints, directions, new TextMap());
+        simulation2.run();
 
         System.out.println("STOP");
     }
---
+++ oolab/src/main/java/agh/ics/oop/model/TextMap.java
@@ -0,0 +1,52 @@
+package agh.ics.oop.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TextMap implements WorldMap<String, Integer> {
+
+    private final List<String> objects = new ArrayList<>();
+
+    @Override
+    public boolean place(String object, Integer position) {
+        objects.add(object);
+        return true;
+    }
+
+    @Override
+    public void move(String object, MoveDirection direction) {
+        int currentIndex = objects.indexOf(object);
+
+        if (currentIndex == -1) return;
+
+        int newIndex = switch (direction) {
+            case FORWARD, RIGHT -> currentIndex + 1;
+            case BACKWARD, LEFT -> currentIndex - 1;
+        };
+
+        if (newIndex >= 0 && newIndex < objects.size()) {
+            String swapped = objects.set(newIndex, object);
+            objects.set(currentIndex, swapped);
+        }
+    }
+
+    @Override
+    public boolean isOccupied(Integer position) {
+        return position >= 0 && position < objects.size();
+    }
+
+    @Override
+    public String objectAt(Integer position) {
+        return isOccupied(position) ? objects.get(position) : null;
+    }
+
+    @Override
+    public boolean canMoveTo(Integer position) {
+        return isOccupied(position);
+    }
+
+    @Override
+    public String toString() {
+        return objects.toString();
+    }
+}
